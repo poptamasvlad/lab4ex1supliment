83704f94bb33d3f4d0db69ca3c3a279fc16f7ffb
Poptamas Vlad <poptamasvlad@gmail.com>
2023-01-15 14:26:41 +0200

    Add project files.

---
+++ lab4ex1supliment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lab4ex1supliment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Se citesc doua numere de la tastatura. Scrieti un program care va calcula
+            cel mai mare divizor comun al numerelor*/
+
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int cmmdc = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if(n % i == 0 && m % i == 0)
+                {
+                    cmmdc = i;
+                }
+            }
+
+            Console.WriteLine("Cel mai mare divizor comun este " + cmmdc);
+            
+        }
+    }
+}
